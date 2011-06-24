1c9006140f53aaa53ff84e6c1af8369d32771bb7
Frederik Mogensen <sodiaf@gmail.com>
2011-06-25 00:02:20 +0200

    Refactored the code, now has an internal api file that can be used in the index.php and in different services..

---
+++ cal-api.php
@@ -0,0 +1,116 @@
+<?php
+
+include('simplehtmldom/simple_html_dom.php');
+include('conf.php');
+
+function curl_download($Url){
+
+    // is cURL installed yet?
+    if (!function_exists('curl_init')){
+	die('Sorry cURL is not installed!');
+    }
+
+    // OK cool - then let's create a new cURL resource handle
+    $ch = curl_init();
+
+    // Now set some options (most are optional)
+
+    // Set URL to download
+    curl_setopt($ch, CURLOPT_URL, $Url);
+
+    // Set a referer
+    curl_setopt($ch, CURLOPT_REFERER, "http://calendar.au.dk/cal.php");
+
+    // User agent
+    curl_setopt($ch, CURLOPT_USERAGENT, "MozillaXYZ/1.0");
+
+    // Include header in result? (0 = yes, 1 = no)
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+
+    // Should cURL return or print out the data? (true = return, false = print)
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Timeout in seconds
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+    // Download the given URL, and return output
+    $output = curl_exec($ch);
+
+    // Close the cURL resource, and free system resources
+    curl_close($ch);
+
+    return $output;
+}
+
+function getRoom($roomId, $date){
+
+    $roomId = urlencode($roomId);
+
+    $redirectPage = curl_download("http://calendar.au.dk/cal.php?res=".$roomId."&view=day");
+    $html = str_get_html($redirectPage);
+
+    // echo htmlspecialchars($html);
+
+    $res = $html->find('meta');
+
+    $url = substr($res[0]->attr['content'], 7);
+
+    if ($date != "") {
+	$calContent= curl_download($url."&date=" . $date);
+    } else {
+	$calContent= curl_download($url);
+    }
+    $html = str_get_html($calContent);
+
+    $ret = $html->find('.swcCCalTableBody');
+
+    foreach($ret as $element) {
+	foreach($element->find('p') as $p){
+	    $pElements[] = trim($p->plaintext);
+	}
+	foreach($element->find('font') as $p){
+	    $pElements[] = trim($p->plaintext);
+	}
+    }
+
+    foreach ($pElements as $key => $value) { 
+	if (is_null($value) || trim($value) === "") { 
+	    unset($pElements[$key]); 
+	} 
+    } 
+
+    return array_unique($pElements);
+}
+
+function getRoomArray($name, $date = "") {
+    foreach( $GLOBALS['confArray'][$name] as $key => $value) {
+	$roomInfo = getRoom($value, $date);
+
+	$resArray[$value]['roomCode'] = $value;
+	$resArray[$value]['name'] = $key;
+
+	foreach($roomInfo as $infoKey => $agenda) {
+
+	    if ( trim($agenda) != "No Meetings"){
+		$resArray[$value]['agendas'][$infoKey]['start'] = date("Hi", strtotime(substr($agenda, 0, 10)) );
+		$resArray[$value]['agendas'][$infoKey]['end']   = date("Hi", strtotime(substr($agenda, 13, 10)) );
+		$resArray[$value]['agendas'][$infoKey]['info']  = trim(substr($agenda, 23));
+	    } else {
+		$resArray[$value]['noMeetings'] = true;
+	    }
+	}
+	if(is_array($resArray[$value]['agendas'])) {
+	    uasort($resArray[$value]['agendas'], cmpAgenda);
+	}
+    }
+    return $resArray;
+}
+
+function cmpAgenda($a, $b){
+    if ($a['start'] == $b['start'] ) {
+	return 0;
+    }
+    return ($a['start'] < $b['start']) ? -1 : 1 ;
+}
+
+?>
---
+++ conf.php
@@ -0,0 +1,23 @@
+<?php
+$confArray['ithus'] = array( 
+	"ITHuset Store Aud" => "DI-5510.103 (Store Aud. Åbogade 15)",
+	"ITHuset Lille Aud" => "DI-5510.104 (Lille Aud. Åbogade 15)",
+	"ITHuset 112" => "DI-5520.112 (Undervisningslokale. Åbogade 15)",
+	"ITHuset 120" => "DI-5523.120",
+	"ITHuset 121" => "DI-5523.121",
+	"ITHuset 129" => "DI-5523.129",
+	"ITHuset 131" => "DI-5523.131",
+	"ITHuset 137 (IMV)" => "DI-5524.137 (IMV)",
+	"ITHuset 139 (IMV)" => "DI-5524.139 (IMV)",
+	"ITHuset 143 (IMV)" => "DI-5524.143 (IMV)",
+	"ITHuset 147 (IMV)" => "DI-5524.147 (IMV)");
+
+$confArray['stibitz'] = array( 
+	"Stibitz 123 (Lærestedet-1)" => "DI-Stibitz-123 (Lærestedet-1)",
+	"Stibitz 113 (Lærestedet-2)" => "DI-Stibitz-113 (Lærestedet-2)",
+	"Stibitz 117 (Lærestedet-gennemgang)" => "DI-Stibitz-117 (Lærestedet-gennemgang)");
+
+$confArray['zuse'] = array( 
+	"Zuse 127 (Øvelser)" => "DI-Zuse-127 (Øvelser)",
+	"Zuse 128c Legolab" => "DI-Zuse-128c Legolab");
+?>
